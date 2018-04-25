39ebd04e525b9e7ab1443deb9db8b9ee1c13035f
lukasz <l.sienko@stud.elka.pw.edu.pl>
2018-04-25 20:01:16 +0200

    decoding

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import edu.stanford.nlp.mt.Phrasal;
+import edu.stanford.nlp.mt.lm.LanguageModel;
+import edu.stanford.nlp.mt.lm.LanguageModelFactory;
 import edu.stanford.nlp.mt.train.AlignmentGrid;
 import edu.stanford.nlp.mt.train.PhraseExtract;
 import edu.stanford.nlp.mt.util.IOTools;
+import edu.stanford.nlp.mt.util.IString;
 import edu.stanford.nlp.util.StringUtils;
 
 import java.io.File;
@@ -14,6 +17,121 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         *
+         *
+         * java
+
+         -server -ea -Xmx${MEM} -Xms${MEM} -XX:+UseParallelGC -XX:+UseParallelOldGC
+
+         -Djava.library.path=/home/lsienko/phrasal/src-cc
+
+         edu.stanford.nlp.mt.Phrasal
+
+         $RUNNAME.ini
+         newstest2012.newstest2011.baseline.ini
+
+         -log-prefix $RUNNAME
+         -log-prefix newstest2012.newstest2011.baseline
+
+
+         <$DECODE_FILE
+
+         >$RUNNAME.trans
+         >newstest2012.newstest2011.baseline.trans
+
+         > logs/$RUNNAME.log
+         >logs/newstest2012.newstest2011.baseline.log
+         *
+         *
+         *
+         * Usage: java edu.stanford.nlp.mt.Phrasal OPTS [ini_file] < input > output
+
+         Phrasal: A phrase-based machine translation decoder from the Stanford NLP group.
+
+         Command-line arguments override arguments specified in the optional ini_file:
+         -text file : Filename of file to decode
+
+         -ttable-file filename : Translation model file. Multiple models can be specified by separating filenames with colons.
+         -lmodel-file filename : Language model file. For KenLM, prefix filename with 'kenlm:'
+         -ttable-limit num : Translation option limit.
+         -n-best-list num : n-best list size.
+         -distinct-n-best-list boolean : Generate distinct n-best lists (default: false)
+         -  -force-decode filename [filename] : Force decode to reference file(s).
+         -prefix-align-compounds boolean : Apply heuristic compound word alignmen for prefix decoding? Affects cube pruning decoder only. (default: false)
+         -stack num : Stack/beam size.
+         -search-algorithm [cube|multibeam] : Inference algorithm (default:cube)
+         -reordering-model type filename [options] : Lexicalized re-ordering model where type is [classic|hierarchical]. Multiple models can be separating filenames with colons.
+         -weights-file filename : Load all model weights from file.
+         -max-sentence-length num : Maximum input sentence length.
+         -min-sentence-length num : Minimum input sentence length.
+         -distortion-limit num [cost] : Hard distortion limit and delay cost (default cost: 0.0).
+         -additional-featurizers class [class] : List of additional feature functions.
+         -disabled-featurizers class [class] : List of baseline featurizers to disable.
+         -threads num : Number of decoding threads (default: 1)
+         -use-itg-constraints boolean : Use ITG constraints for decoding (multibeam search only)
+         -recombination-mode name : Recombination mode [pharoah,exact,dtu] (default: exact).
+         -drop-unknown-words boolean : Drop unknown source words from the output (default: false)
+         -independent-phrase-tables filename [filename] : Phrase tables that cannot have associated reordering models. Optionally supports custom per-table prefixes for features (e.g., pref:filename).
+         -alignment-output-file filename : Output word-word alignments to file for each translation.
+         -preprocessor-filter language [opts] : Pre-processor to apply to source input.
+         -postprocessor-filter language [opts] : Post-processor to apply to target output.
+         -source-class-map filename : Feature API: Line-delimited source word->class mapping (TSV format).
+         -target-class-map filename : Feature API: Line-delimited target word->class mapping (TSV format).
+         -gaps options : DTU: Enable Galley and Manning (2010) gappy decoding.
+         -max-pending-phrases num : DTU: Max number of pending phrases for decoding.
+         -gaps-in-future-cost boolean : DTU: Allow gaps in future cost estimate (default: true)
+         -linear-distortion-options type : DTU: linear distortion type (default: standard)
+         -print-model-scores boolean : Output model scores with translations (default: false)
+         -input-properties file : File specifying properties of each source input.
+         -feature-augmentation mode : Feature augmentation mode [all|dense|extended].
+         -wrap-boundary boolean : Add boundary tokens around each input sentence (default: false).
+         -ksr_nbest_size int : size of n-best list for KSR computation (default: 0, i.e. no KSR computation).
+         -wpa_nbest_size int : size of n-best list for word prediction accuracy computation (default: 0, i.e. no WPA computation).
+         -reference String : reference file for KSR/WPA computation.
+         */
+        try {
+
+            File ini_file = new File("resources/phrasal.ini");
+            String ini_file_path = ini_file.getAbsolutePath();
+
+            File phrase_table_file = new File("resources/translation_model/phrase-table.gz");
+            String phrase_table_file_path = phrase_table_file.getAbsolutePath();
+
+            File lang_model_file = new File("resources/language_model/5gm.bin");
+            String lang_model_file_path = lang_model_file.getAbsolutePath();
+
+            String[] decode_args = new String[13];
+            decode_args[0] = "-ttable-file";
+            decode_args[1] = phrase_table_file_path;
+            decode_args[2] = "-lmodel-file";
+            decode_args[3] = "kenlm:"+lang_model_file_path;
+            decode_args[4] = "-ttable-limit";
+            decode_args[5] = "20";
+            decode_args[6] = "-distortion-limit";
+            decode_args[7] = "5";
+            decode_args[8] = "-threads";
+            decode_args[9] = "2";
+
+            File reordering_model_file = new File("resources/translation_model/lo-hier.msd2-bidirectional-fe.gz");
+            String reordering_model_file_path = reordering_model_file.getAbsolutePath();
+
+            decode_args[10] = "-reordering-model";
+            decode_args[11] = "msd2-bidirectional-fe" + " " + reordering_model_file_path;
+            //decode_args[11] = "hierarchical";
+            decode_args[12] = ini_file_path;
+
+
+            //Phrasal.main(decode_args);
+            decode(decode_args);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void buildTranslationModel() {
         // BUILDING TRANSLATION MODEL (PHRASE TABLE EXTRACTION)
         File berkeley_aligner_folder = new File("resources/aligner_output");
         String aligner_folder_path = berkeley_aligner_folder.getAbsolutePath();
@@ -44,10 +162,6 @@ public class Main {
         phrase_extract_args[16] = "-maxELen";
         phrase_extract_args[17] = "5";
 
-        //TODO: check why it did not calculate itself
-        //phrase_extract_args[12] = "-maxLen";
-        //phrase_extract_args[13] = "256";
-
         System.setProperty("ShowPhraseRestriction", "true");
 
         try {
@@ -60,14 +174,26 @@ public class Main {
     private static void decode(String[] args) throws Exception {
         final Properties options = StringUtils.argsToProperties(args);
         final String configFile = options.containsKey("") ? (String) options.get("") : null;
+        options.remove("");
+
+        // by default, exit on uncaught exception
+        Thread.setDefaultUncaughtExceptionHandler((t, ex) -> {
+            System.err.println("Uncaught top-level exception");
+            ex.printStackTrace();
+            System.exit(-1);
+        });
 
         final Map<String, List<String>> configuration = getConfigurationFrom(configFile, options);
-        final Phrasal p = Phrasal.loadDecoder(configuration);
 
-        //if (options.containsKey("text")) p.decode(new FileInputStream(new File(options.getProperty("text"))), true);
-        //else p.decode(System.in, true);
+        System.out.println("przed load decoder");
+        LanguageModel<IString> lm = LanguageModelFactory.load(options.getProperty("lmodel-file"));
+        System.out.println("pomiedzy");
+        final Phrasal p = Phrasal.loadDecoder(configuration, lm);
+        System.out.println("po load decoder");
 
         p.decode(System.in, true);
+        /*if (options.containsKey("text")) p.decode(new FileInputStream(new File(options.getProperty("text"))), true);
+        else p.decode(System.in, true);*/
     }
 
     private static Map<String, List<String>> getConfigurationFrom(String configFile, Properties options)
